ecc02d64a2296ee17cbe08735f1cc3f6ec8d608b
shubham2o <2contactshubham@gmail.com>
2024-07-05 13:51:22 +0530

    Completed - Section 2: Building Components

--- Sheryians CS/Sheryians-React/src/Components/Card.jsx
+++ Sheryians CS/Sheryians-React/src/Components/Card.jsx
@@ -1,7 +1,25 @@
 const Card = () => {
     return (
-        <div className="bg-red-800">
-            <h1>Heading1</h1>
+        <div className="w-full h-screen bg-zinc-700">
+            <div
+                className="w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-xl overflow-hidden"
+            >
+                <div className="w-full h-32 bg-zinc-300">
+                    <img
+                        src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1hem9ufGVufDB8fDB8fHww"
+                        className="w-full h-full object-cover"
+                        alt="Amazon-Basics-img"
+                    />
+                </div>
+                <div className="w-full px-3 py-4">
+                    <h2 className="font-semibold">
+                        Amazon Basics
+                    </h2>
+                    <p className="text-xs mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
