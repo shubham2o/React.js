1b87ae5b87d63ecff207bac966663e809209217b
shubham2o <2contactshubham@gmail.com>
2024-07-13 12:48:25 +0530

    Completed - Section 9: Props and State Exercise

--- Sheryians CS/Sheryians-React/src/Components/Navbar.jsx
+++ Sheryians CS/Sheryians-React/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = ({ data }) => {
         <div className="w-full px-8 py-3 flex justify-between items-center font-semibold">
             <h3 className="text-orange-600 underline decoration-2 font-bold text-lg">Playlist.</h3>
 
-            <div className="w-[7.2rem] px-2 py-1 bg-orange-600 text-white rounded-md text-sm flex justify-between items-center gap-1.5">
+            <div className="w-[8.5rem] px-2 py-1 bg-orange-600 text-white rounded-md text-sm flex justify-between items-center gap-1.5">
                 <h3 className="pt-0.5">Favourites</h3>
 
                 <h4
