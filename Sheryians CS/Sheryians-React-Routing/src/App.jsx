f483484a95e4f7ba181421ce872a535edc9a2851
shubham2o <2contactshubham@gmail.com>
2024-07-16 00:03:16 +0530

    Basic installation and setup completed

---
+++ Sheryians CS/Sheryians-React-Routing/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="w-full h-screen bg-zinc-800 text-white">
+        App Component.
+      </h1>
+    </div>
+  )
+}
+
+export default App;
